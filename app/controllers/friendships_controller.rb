4afb023ad5558d64fb5c87ea44013a09acddee48
Yanick <tyankee15@gmail.com>
2020-11-25 09:18:34 -0300

    refactor frienship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,9 @@
 class FriendshipsController < ApplicationController
     def create
-        @user = User.find(params[:user_id])
+        # @user = User.find(params[:user_id])
 
-        friendship =  @user.friendships.build(params[:friend_id])
-        friendship.status = false
-        friendship.save
+        @friendship =  current_user.friendships.build(friend_id:params[:friend_id], status:false)
+        @friendship.save
+        redirect_to users_path, notice: 'Friendship request was sented.'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :users, only: [:index, :show] 
   resources :friendships, only: [:create, :destroy]
-  resources :users, only: [:index, :show]
+  
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
