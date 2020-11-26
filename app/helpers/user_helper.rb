00a55ecbaa86d559130ab5941d8851c04e7cb6e4
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-11-27 00:31:53 +0200

    move application logic from helper to model

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,28 +1,20 @@
 module UserHelper
-  def friendship_with(user)
-    current_user.friendships.find_by_friend_id(user.id)
-  end
-
-  def inverse_friendship_with(user)
-    current_user.inverse_friendships.find_by_user_id(user.id)
-  end
-
   def friendship_sender_side(user)
-    if friendship_with(user).nil?
+    if current_user.friendship_with(user).nil?
       link_to("Invite #{user.name}",
               friendships_path(friend_id: user.id),
               method: :post,
               class: 'button')
 
-    elsif !friendship_with(user).status
+    elsif !current_user.friendship_with(user).status
       link_to('delete request',
-              friendship_path(friendship_with(user)),
+              friendship_path(current_user.friendship_with(user)),
               method: :delete,
               class: 'button')
 
     elsif current_user.friend?(user)
       link_to('Unfriend',
-              friendship_path(friendship_with(user)),
+              friendship_path(current_user.friendship_with(user)),
               method: :delete,
               class: 'button')
     end
@@ -31,24 +23,24 @@ module UserHelper
   def friendship_reciever_side(user)
     if current_user.friend?(user)
       link_to('Unfriend',
-              friendship_path(inverse_friendship_with(user)),
+              friendship_path(current_user.inverse_friendship_with(user)),
               method: :delete,
               class: 'button')
     else
       link_to('Accept',
-              accept_path(friendship_id: inverse_friendship_with(user).id),
+              accept_path(friendship_id: current_user.inverse_friendship_with(user).id),
               method: :post,
               class: 'button')\
         .concat(' ')\
         .concat(link_to('Reject',
-                        friendship_path(inverse_friendship_with(user)),
+                        friendship_path(current_user.inverse_friendship_with(user)),
                         method: :delete,
                         class: 'button'))
     end
   end
 
   def friendship?(user)
-    if inverse_friendship_with(user).nil?
+    if current_user.inverse_friendship_with(user).nil?
       friendship_sender_side(user)
     else
       friendship_reciever_side(user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,12 @@ class User < ApplicationRecord
   def friend?(user)
     friends_list.include?(user)
   end
+
+  def friendship_with(user)
+    friendships.find_by_friend_id(user.id)
+  end
+
+  def inverse_friendship_with(user)
+    inverse_friendships.find_by_user_id(user.id)
+  end
 end
