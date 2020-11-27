eb6151b6354bfa55c4d2e30cff0a50ea35c8428b
Yanick <tyankee15@gmail.com>
2020-11-27 10:06:53 -0300

    Add mutual friends private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,27 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @mutual_friends = User.where(id: show_three_friends)
   end
 
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+  end
+
+  private
+
+  def show_mutual_friends
+    @ids = []
+    current_user.friends.each do |people|
+      people.friends.each do |x|
+        @ids << x.id
+      end
+    end
+    @ids.reject { |x| x == current_user.id }
+  end
+
+  def show_three_friends
+    show_mutual_friends.sample(3)
   end
 end
