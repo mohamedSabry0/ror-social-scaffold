67a0217bc9d77a98917cfd6173408735a84f70bb
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-11-27 22:55:14 +0200

    moving some logic from controller to model

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,27 +1,19 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id], status: false)
-    @friendship.save
+    friendship = current_user.friendships.build(friend_id: params[:friend_id], status: false)
+    friendship.save
     redirect_to users_path, notice: 'Friendship request was sented.'
   end
 
   def destroy
-    @friendship1 = Friendship.find(params[:id])
-    @friendship2 = Friendship.find_by_friend_id(@friendship1.user_id)
-    @friendship1.destroy
-    @friendship2&.destroy
+    friendship = Friendship.find(params[:id])
+    friendship.remove_friendship
     redirect_to users_path, notice: 'Friendship was deleted.'
   end
 
   def accept
     friendship = Friendship.find(params[:friendship_id])
-    friendship.status = true
-    friendship.save
-    Friendship.create(
-      user_id: friendship.friend_id,
-      friend_id: friendship.user_id,
-      status: true
-    )
+    friendship.confirm_friendship
     redirect_to users_path, notice: 'Friendship request was accepted.'
   end
 end
